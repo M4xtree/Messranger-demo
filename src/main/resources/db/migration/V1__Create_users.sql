1efa91146c69d542eca4d0bb97e20ef8990bba1e
M4xtree <maxtree1903@gmail.com>
2024-10-14 13:28:30 +0300

    5 commit

--- src/main/resources/db/migration/V1__Create_users.sql
+++ src/main/resources/db/migration/V1__Create_users.sql
@@ -7,7 +7,7 @@ CREATE TABLE users (
 CREATE TABLE chat (
     id VARCHAR PRIMARY KEY,
     type VARCHAR NOT NULL,
-    created_by INT NOT NULL REFERENCES users(id),
+    created_by VARCHAR NOT NULL REFERENCES users(id),
     name VARCHAR,
     description VARCHAR,
     is_private BOOLEAN DEFAULT FALSE,
@@ -17,7 +17,7 @@ CREATE TABLE chat (
 CREATE TABLE messages (
     id VARCHAR PRIMARY KEY,
     chat_id VARCHAR NOT NULL REFERENCES chat(id),
-    sender_id VARCHAR NOT NULL REFERENCES user(id),
+    sender_id VARCHAR NOT NULL REFERENCES users(id),
     content VARCHAR NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     is_deleted BOOLEAN DEFAULT FALSE,
@@ -28,8 +28,8 @@ CREATE TABLE messages (
 
 CREATE TABLE members (
     chat_id VARCHAR NOT NULL REFERENCES chat(id),
-    user_id VARCHAR NOT NULL REFERENCES user(id),
-    role VARCHAR(20) DEFAULT 'member'
+    user_id VARCHAR NOT NULL REFERENCES users(id),
+    role VARCHAR DEFAULT 'member',
     can_delete_messages BOOLEAN DEFAULT FALSE,
     can_add_participants BOOLEAN DEFAULT FALSE,
     can_edit_messages BOOLEAN DEFAULT FALSE,
