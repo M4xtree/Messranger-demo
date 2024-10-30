871e395831daaa81902fad307c5b26a122f0ca1c
M4xtree <maxtree1903@gmial.com>
2024-10-30 22:44:11 +0300

    16s commit

--- src/main/java/com/messranger/services/UserService.java
+++ src/main/java/com/messranger/services/UserService.java
@@ -1,11 +1,9 @@
 package com.messranger.services;
 
 import com.messranger.entity.User;
-import com.messranger.model.PageRequest;
 import com.messranger.repositories.UserRepository;
 
-import java.util.List;
-import java.util.Optional;
+
 
 public class UserService extends BaseService<User>{
     public UserService() {
