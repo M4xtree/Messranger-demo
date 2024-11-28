f5059427d853ef391268897325043fa777d6aefc
M4xtree <maxtree1903@gmial.com>
2024-11-28 11:08:27 +0300

    19s commit

--- src/main/java/com/messranger/repositories/MembersRepository.java
+++ src/main/java/com/messranger/repositories/MembersRepository.java
@@ -27,12 +27,12 @@ public class MembersRepository extends BaseRepository<Members> {
 
     @Override
     protected String[] getColumnNames() {
-        return new String[]{"role", "can_delete_messages", "can_add_participants", "can_edit_messages", "caret", "joined_at", "chat_id", "user_id"};
+        return new String[]{"role", "can_delete_messages", "can_add_participants", "can_edit_messages", "caret", "joined_at"};
     }
 
     @Override
     protected String[] getColumnPlaceholders() {
-        return new String[]{"?", "?", "?", "?", "?", "?", "?", "?"};
+        return new String[]{"?", "?", "?", "?", "?", "?"};
     }
 
     @Override
